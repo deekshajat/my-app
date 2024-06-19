8b3e7ad88465fa3c19c72c9c90c345f19fd71eba
Deeksha <deekshajat95@gmail.com>
2024-06-20 01:15:44 +0530

    Removed Routing Part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
@@ -41,24 +41,24 @@ function App() {
     <>
       {/* <Navbar title="TextUtils2" aboutText="About textUtils" /> */}
       {/* <Navbar /> */}
-      <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Switch>
+      {/* <Router> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Switch>
             <Route exact path="/about">
               <About />
             </Route>
-            <Route exact path="/">
-              <TextForm
-                showAlert={showAlert}
-                heading="Enter the text to analyze"
-                mode={mode}
-              />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+            <Route exact path="/"> */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze"
+          mode={mode}
+        />
+        {/* </Route>
+          </Switch> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
