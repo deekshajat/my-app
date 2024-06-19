475880bc07c3acd315a94a56fc49bd03f0494cd6
Deeksha <deekshajat95@gmail.com>
2024-06-20 01:37:31 +0530

    Added TextUtils

---
+++ module1.mjs
@@ -0,0 +1,2 @@
+import a, { b, c, d } from "./module2.mjs";
+console.log(a, b, c, d);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Deeksha";
+const b = "Biswa";
+const c = "Ashna";
+const d = "Akash";
+
+export default a;
+export { b };
+export { c };
+export { d };
